883a638e2ac0e97b62ceb200e7aad239b42c6031
Fernando Moreira <nandomoreira.me@gmail.com>
2015-09-22 16:54:24 -0300

    update version 2.0

--- app/index.js
+++ app/index.js
@@ -1,11 +1,8 @@
 'use strict';
 
-var util = require('util');
-var path = require('path');
 var yeoman = require('yeoman-generator');
 var generators = yeoman.generators;
 var yosay = require('yosay');
-var chalk = require('chalk');
 
 var GulpBoilerplateGenerator = generators.Base.extend({
 
@@ -18,25 +15,30 @@ var GulpBoilerplateGenerator = generators.Base.extend({
   scaffoldFolders: function () {
     this.mkdir('gulp');
     this.mkdir('gulp/tasks');
-
     this.mkdir('src');
     this.mkdir('src/sass');
     this.mkdir('src/js');
     this.mkdir('src/images');
     this.mkdir('src/fonts');
+    this.mkdir('src/jade');
+    this.mkdir('src/jade/layouts');
+    this.mkdir('src/jade/views');
   },
 
   appFiles: function () {
     this.directory('gulp/tasks');
     this.directory('src/sass');
     this.directory('src/js');
+    this.directory('src/jade');
     this.directory('src/images');
     this.directory('src/fonts');
   },
 
   projectfiles: function () {
+    this.copy('gulp/config.json', 'gulp/config.json');
     this.copy('README.md', 'README.md');
-    this.copy('index.html', 'index.html');
+    this.copy('Gemfile', 'Gemfile');
+    this.copy('config.rb', 'config.rb');
     this.copy('gulpfile.js', 'gulpfile.js');
     this.copy('package.json', 'package.json');
   },
@@ -46,7 +48,7 @@ var GulpBoilerplateGenerator = generators.Base.extend({
   },
 
   end: function () {
-    console.log('\nPlease wait, installing npm dependencies...\n');
+    console.log('\nPlease wait, installing node dev dependencies...\n');
   }
 });
 
