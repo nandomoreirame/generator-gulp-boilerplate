39f2077bf074cb02272df09a9af8acda3271fab6
Fernando Moreira <nandomoreira.me@gmail.com>
2015-09-23 10:51:47 -0300

    add .gitignore file..

--- app/index.js
+++ app/index.js
@@ -36,6 +36,7 @@ var GulpBoilerplateGenerator = generators.Base.extend({
 
   projectfiles: function () {
     this.copy('gulp/config.json', 'gulp/config.json');
+    this.copy('.gitignore', '.gitignore');
     this.copy('README.md', 'README.md');
     this.copy('Gemfile', 'Gemfile');
     this.copy('config.rb', 'config.rb');
